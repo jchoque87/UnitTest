279c89775a1935aabc5776869ead49328c26d6d2
Javier Choque <javier.dreivaj@gmail.com>
2021-07-05 22:41:45 -0400

    Add Unit Test

--- test/DegreeCalculator.test.js
+++ test/DegreeCalculator.test.js
@@ -11,10 +11,22 @@ test("Given a number 36 in fahrenheit degree the getFahrenheitToCelsius function
     expect(degreeCalculatorObj.getFahrenheitToCelsius()).toBe(2.2222222222222223);
 }); 
 
+test("Given a number 2.2222222222222223 in celsius degree the getCelsiusToFahrenheit function it should be return a number 36 in fahrenheit value degree", ()=>{
+    const degreeCalculatorObj = new DegreeCalculator(2.2222222222222223, 'celsius', 'fahrenheit');
+    expect(degreeCalculatorObj.getCelsiusToFahrenheit()).toBe(36);
+}); 
+
+
 test("Given a number 0 in kelvin degree the getKelvinToCelsius function it should be return a number -273.15 in celsius value degree", ()=>{
     const degreeCalculatorObj = new DegreeCalculator(0, 'kelvin', 'celsius');
     expect(degreeCalculatorObj.getKelvinToCelsius()).toBe(-273.15);
 }); 
+
+test("Given a number -273.15 in celsius degree the getCelsiusToKelvin function it should be return a number 0 in celsius value degree", ()=>{
+    const degreeCalculatorObj = new DegreeCalculator(-273.15, 'celsius', 'kelvin');
+    expect(degreeCalculatorObj.getCelsiusToKelvin()).toBe(0);
+}); 
+
 
 test("Given a number 100 in celsius degree the getCelsiusToKelvin function it should be return a number 373.15 in kelvin value degree", ()=>{
     const degreeCalculatorObj = new DegreeCalculator(100, 'celsius', 'kevin');
